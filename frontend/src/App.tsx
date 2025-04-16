aa83ff761ab35c94266a76cdc18f1736f27752e0
Quarktum <huynhhoangphuc25@gmail.com>
2025-04-16 16:29:02 +1000

    feat: implement company profile page, add routing, and integrate react-icons for improved UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,75 +1,14 @@
-import React, { ChangeEvent, SyntheticEvent, use, useState } from 'react';
-import logo from './logo.svg';
-import CardList from './Components/CardList/CardList';
+import { Outlet } from 'react-router';
 import './App.css';
-import Search from './Components/Search/Search';
-import { CompanySearch } from './company';
-import { searchCompanies } from './Components/api';
-import ListPortfolio from './Components/Portfolio/ListPortfolio/ListPortfolio';
 import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/Hero/Hero';
 
 function App() {
-  const [search, setSearch] = useState<string>("");
-  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
-  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [serverError, setServerError] = useState<string>("");
 
-  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    console.log(e);
-  };
-
-  const onSearchSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-
-    const result = await searchCompanies(search);
-    if (typeof result === "string") {
-      setServerError(result);
-    } else if (Array.isArray(result.data)) {
-      setSearchResult(result.data)
-    }
-
-    console.log(searchResult);
-  };
-
-  const onPortfolioCreate = (e: any) => {
-    e.preventDefault();
-
-    const exists = portfolioValues.find((value) => value === e.target[0].value);
-    if (exists)
-      return;
-
-    const updatedPortfolio = [...portfolioValues, e.target[0].value];
-    setPortfolioValues(updatedPortfolio);
-  }
-
-  const onPortfolioDelete = (e: any) => {
-    e.preventDefault();
-    console.log(e);
-    const removed = portfolioValues.filter((value) => {
-      return value !== e.target[0].value;
-    });
-    setPortfolioValues(removed);
-  }
 
   return (
     <>
       <Navbar />
-      <Search
-        onSearchSubmit={onSearchSubmit}
-        search={search}
-        handleSearchChange={handleSearchChange} />
-
-      <Hero />
-      <ListPortfolio
-        portfolioValues={portfolioValues}
-        onPortfolioDelete={onPortfolioDelete}
-      />
-      <CardList
-        searchResults={searchResult}
-        onPortfolioCreate={onPortfolioCreate} />
-      {serverError && <h1>{serverError}</h1>}
+      <Outlet />
     </>
   );
 }
--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -3,6 +3,7 @@ import './Card.css';
 import { compileFunction } from 'vm';
 import { CompanySearch } from '../../company';
 import AddPortfolio from '../Portfolio/AddPortfolio/AddPortfolio';
+import { Link } from 'react-router-dom';
 
 interface Props {
     id: string;
@@ -17,9 +18,9 @@ const Card: React.FC<Props> = ({ id, searchResult, onPortfolioCreate }: Props): 
             key={id}
             id={id}
         >
-            <h2 className="font-bold text-center text-veryDarkViolet md:text-left">
+            <Link to={`/company/${searchResult.symbol}`} className="font-bold text-center text-veryDarkViolet md:text-left">
                 {searchResult.name} ({searchResult.symbol})
-            </h2>
+            </Link>
             <p className="text-veryDarkBlue">{searchResult.currency}</p>
             <p className="font-bold text-veryDarkBlue">{searchResult.exchange} - {searchResult.exchangeFullName}</p>
 
