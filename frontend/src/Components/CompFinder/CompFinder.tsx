cb309594a09c6eec99bf8cca7093f474fb18bb4a
Quarktum <huynhhoangphuc25@gmail.com>
2025-04-22 15:14:53 +1000

    feat: add CompFinder and CompFinderItem components with API integration for company data retrieval

---
+++ frontend/src/Components/CompFinder/CompFinder.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { CompanyCompData } from '../../company';
+import { getCompData } from '../api';
+import CompFinderItem from './CompFinderItem/CompFinderItem';
+
+type Props = {
+    ticker:string;
+}
+
+const CompFinder = ({ticker}: Props) => {
+    const [companyData, setCompanyData] = useState<CompanyCompData[]>();
+    useEffect(() => {
+        const getComp = async () => {
+            const value = await getCompData(ticker);
+            setCompanyData(value?.data);
+        }
+        getComp();
+    }, [ticker]);
+    
+  return (
+    <div className='inline-flex round-md shadow-sm m-4'>
+        {companyData?.map((ticker:CompanyCompData) => {
+            return <CompFinderItem ticker={ticker.symbol}/>
+        })}
+    </div>
+  )
+}
+
+export default CompFinder
--- frontend/src/Components/api.tsx
+++ frontend/src/Components/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { CompanyBalanceSheet, CompanyCashFlow, CompanyIncomeStatement, CompanyKeyMetrics, CompanyKeyRatios, CompanyProfile, CompanySearch } from "../company"
+import { CompanyBalanceSheet, CompanyCashFlow, CompanyCompData, CompanyIncomeStatement, CompanyKeyMetrics, CompanyKeyRatios, CompanyProfile, CompanySearch } from "../company"
 
 interface SearchResponse {
     data: CompanySearch[];
@@ -71,6 +71,17 @@ export const getCashFlow = async (query: string) => {
     try {
         const data = await axios.get<CompanyCashFlow[]>(
             `https://financialmodelingprep.com/stable/cash-flow-statement?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
+        );
+        return data;
+    } catch (error: any) {
+        console.log("error message from API: ", error.message);
+    }
+}
+
+export const getCompData = async (query: string) => {
+    try {
+        const data = await axios.get<CompanyCompData[]>(
+            `https://financialmodelingprep.com/stable/stock-peers?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
         );
         return data;
     } catch (error: any) {
