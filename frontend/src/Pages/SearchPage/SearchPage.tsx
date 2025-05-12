3ea4a47d09d4d94318dd5d44be2b6383eff7b4fc
Quarktum <huynhhoangphuc25@gmail.com>
2025-05-12 17:18:55 +1000

    feat: refactor portfolio management by introducing PortfolioGet model, updating components to use it, and implementing API services for portfolio operations

--- frontend/src/Pages/SearchPage/SearchPage.tsx
+++ frontend/src/Pages/SearchPage/SearchPage.tsx
@@ -1,16 +1,25 @@
-import React, { useState, useEffect, SyntheticEvent, ChangeEvent } from 'react'
-import Search from '../../Components/Search/Search'
-import Hero from '../../Components/Hero/Hero'
-import ListPortfolio from '../../Components/Portfolio/ListPortfolio/ListPortfolio'
-import CardList from '../../Components/CardList/CardList'
-import { searchCompanies } from '../../Components/api'
-import { CompanySearch } from '../../company'
+import React, { useState, useEffect, SyntheticEvent, ChangeEvent } from "react";
+import Search from "../../Components/Search/Search";
+import Hero from "../../Components/Hero/Hero";
+import ListPortfolio from "../../Components/Portfolio/ListPortfolio/ListPortfolio";
+import CardList from "../../Components/CardList/CardList";
+import { searchCompanies } from "../../Components/api";
+import { CompanySearch } from "../../company";
+import { PortfolioGet } from "../../Models/Portfolio";
+import {
+  portfolioAddAPI,
+  portfolioDeleteAPI,
+  portfolioGetAPI,
+} from "../../Services/PortfolioService";
+import { toast } from "react-toastify";
 
 interface Props {}
 
 const SearchPage = (props: Props) => {
   const [search, setSearch] = useState<string>("");
-  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
+  const [portfolioValues, setPortfolioValues] = useState<PortfolioGet[] | null>(
+    []
+  );
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string>("");
 
@@ -19,6 +28,10 @@ const SearchPage = (props: Props) => {
     console.log(e);
   };
 
+  useEffect(() => {
+    getPortfolio();
+  }, []);
+
   const onSearchSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
 
@@ -26,50 +39,73 @@ const SearchPage = (props: Props) => {
     if (typeof result === "string") {
       setServerError(result);
     } else if (Array.isArray(result.data)) {
-      setSearchResult(result.data)
+      setSearchResult(result.data);
     }
 
     console.log(searchResult);
   };
 
+  const getPortfolio = () => {
+    portfolioGetAPI()
+      .then((res) => {
+        if (res?.data) {
+          setPortfolioValues(res?.data);
+        }
+      })
+      .catch((e) => {
+        toast.warning("Could not get portfolio values!");
+      });
+  };
+
   const onPortfolioCreate = (e: any) => {
     e.preventDefault();
-
-    const exists = portfolioValues.find((value) => value === e.target[0].value);
-    if (exists)
-      return;
-
-    const updatedPortfolio = [...portfolioValues, e.target[0].value];
-    setPortfolioValues(updatedPortfolio);
-  }
+    portfolioAddAPI(e.target[0].value)
+      .then((res) => {
+        if (res?.status === 201) {
+          toast.success("Stock added to portfolio!");
+          getPortfolio();
+        }
+      })
+      .catch((e) => {
+        toast.warning("Could not crate portfolio item!");
+      });
+  };
 
   const onPortfolioDelete = (e: any) => {
     e.preventDefault();
-    console.log(e);
-    const removed = portfolioValues.filter((value) => {
-      return value !== e.target[0].value;
-    });
-    setPortfolioValues(removed);
-  }
+
+    portfolioDeleteAPI(e.target[0].value)
+      .then((res) => {
+        if (res?.status === 200) {
+          toast.success("Stock deleted from portfolio!");
+          getPortfolio();
+        }
+      })
+      .catch((e) => {
+        toast.warning("Could not delete portfolio item!");
+      });
+  };
 
   return (
     <>
       <Search
         onSearchSubmit={onSearchSubmit}
         search={search}
-        handleSearchChange={handleSearchChange} />
+        handleSearchChange={handleSearchChange}
+      />
 
       <Hero />
       <ListPortfolio
-        portfolioValues={portfolioValues}
+        portfolioValues={portfolioValues!}
         onPortfolioDelete={onPortfolioDelete}
       />
       <CardList
         searchResults={searchResult}
-        onPortfolioCreate={onPortfolioCreate} />
+        onPortfolioCreate={onPortfolioCreate}
+      />
       {serverError && <h1>{serverError}</h1>}
-    </>  
-    )
-}
+    </>
+  );
+};
 
-export default SearchPage
+export default SearchPage;
