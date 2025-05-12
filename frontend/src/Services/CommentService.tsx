52e2a9e3a5135919d6fdcf20ba5fdd64c0a904f5
Quarktum <huynhhoangphuc25@gmail.com>
2025-05-12 16:33:19 +1000

    feat: implement StockComment and StockCommentList components with comment retrieval and posting functionality

--- frontend/src/Services/CommentService.tsx
+++ frontend/src/Services/CommentService.tsx
@@ -1,16 +1,25 @@
 import axios from "axios";
-import { CommentPost } from "../Models/Comment";
+import { CommentGet, CommentPost } from "../Models/Comment";
 import { handleError } from "../Helpers/ExportHandler";
 
-const api = "http://localhost:5165/api/comment/";
+const api = "http://localhost:5165/api/comment";
 
 export const commentPostAPI = async (title: string, content: string, symbol: string) => {
     try {
-        const data = await axios.post<CommentPost>(api + `${symbol}`, {
+        const data = await axios.post<CommentPost>(api + `/${symbol}`, {
             title: title,
             content: content,
         })
 
+        return data;
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+export const commentGetAPI = async (symbol: string) => {
+    try {
+        const data = await axios.get<CommentGet[]>(api + `?Symbol=${symbol}`);
         return data;
     } catch (error) {
         handleError(error);
